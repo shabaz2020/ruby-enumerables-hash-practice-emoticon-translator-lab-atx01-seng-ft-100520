e4d6e5e4d39bbbe0b3b6cb2c655fb0c6dc7a6e03
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 20:52:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,42 +1,32 @@
 # require modules here
 require "yaml"
+require 'pry'
 
-def load_library(yaml_file)
-  response = {"get_meaning" => {}, "get_emoticon" => {}}
-  library = YAML.load_file(yaml_file)
-  library.each do |trans, emos|
-    response["get_meaning"][emos[1]] = trans
-    response["get_emoticon"][emos[0]] = emos[1]
+def load_library(emoticon_file)
+  emoticons = YAML.load_file('./lib/emoticons.yml')
+
+  emoticon_lib = {'get_meaning'  => {},
+                  'get_emoticon' => {} }
+
+  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
+    english = value[0]
+    japanese = value[1]
+    emoticon_lib['get_meaning'][japanese] = meaning
+    emoticon_lib['get_emoticon'][english] = japanese
   end
-  response
+  emoticon_lib
 end
 
-def get_japanese_emoticon(yaml_file, emoticon)
-  library = load_library(yaml_file)
-  response = nil
-  library["get_emoticon"].each do |english, japanese|
-    if emoticon == english
-      response = japanese
-    end
-  end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
+
+def get_japanese_emoticon(emoticon_file, emoticon)
+  emoticon_lib = load_library(emoticon_file)
+  japanese_emoticon = emoticon_lib['get_emoticon'][emoticon]
+  japanese_emoticon ? japanese_emoticon : 'Sorry, that emoticon was not found'
 end
 
-def get_english_meaning(yaml_file, emoticon)
-  library = load_library(yaml_file)
-  response = nil
-  library["get_meaning"].each do |emo, trans|
-    if emo == emoticon
-      response = trans
-    end
-  end
-  if response == nil
-    return "Sorry, that emoticon was not found"
-  else
-    return response
-  end
+
+def get_english_meaning(emoticon_file, emoticon)
+  emoticon_lib = load_library(emoticon_file)
+  english_meaning = emoticon_lib['get_meaning'][emoticon]
+  english_meaning ? english_meaning : 'Sorry, that emoticon was not found'
 end
