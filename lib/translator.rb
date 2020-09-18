446efac7c3793e147e2acbb193f7cb660786764a
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 21:25:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,8 @@ def load_library(path)
   final_hash = {}
   YAML.load_file(path).each do |key, value|
      final_hash[key] = {}  
+     final_hash[key][:english] = ""
+     final_hash[key][:japanese] = ""
   end
   final_hash
 end
