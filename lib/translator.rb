a6b6cadfd2d088dff637a95f17364c099930835b
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 21:33:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -13,7 +13,9 @@ end
 
 def get_english_meaning(path, emoticon)
   load_library(path).each do |key, value|
-    binding.pry
+    if value[:japanese] == emoticon
+      return key
+    end
   end
 end
 
