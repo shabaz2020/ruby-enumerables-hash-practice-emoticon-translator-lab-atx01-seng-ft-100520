053517c5768caaa3b59f303fbf3604e2d1ec5c59
scruffy-prompt-3476 <shabaz80@live.com>
2020-09-18 20:49:42 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,22 +1,15 @@
 # require modules here
 requre "yaml"
-require 'pry'
 
-def load_library(emoticon_file)
-  emoticons = YAML.load_file('./lib/emoticons.yml')
-
-  emoticon_lib = {'get_meaning'  => {},
-                  'get_emoticon' => {} }
-
-  emoticons.each do |meaning, value|  # meaning= "surprised"   # value= [":o", "o_O"]  <-(e,j)
-    english = value[0]
-    japanese = value[1]
-    emoticon_lib['get_meaning'][japanese] = meaning
-    emoticon_lib['get_emoticon'][english] = japanese
+def load_library(yaml_file)
+  response = {"get_meaning" => {}, "get_emoticon" => {}}
+  library = YAML.load_file(yaml_file)
+  library.each do |trans, emos|
+    response["get_meaning"][emos[1]] = trans
+    response["get_emoticon"][emos[0]] = emos[1]
   end
-  emoticon_lib
+  response
 end
-
 
 def get_japanese_emoticon(emoticon_file, emoticon)
   emoticon_lib = load_library(emoticon_file)
